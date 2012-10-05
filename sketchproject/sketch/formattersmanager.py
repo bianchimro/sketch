f24dd9597e6d8fc7b88a6a7d2ce13da471aefe6f
Mauro Bianchi <bianchimro@gmail.com>
2012-10-05 12:57:39 +0200

    comments

--- sketchproject/sketch/formattersmanager.py
+++ sketchproject/sketch/formattersmanager.py
@@ -1,16 +1,30 @@
+"""
+This module contains an instance of RecordFormatter,
+to be used to register formatting funcions
+"""
+
 from recordformatter import RecordFormatter
 import operator
+
 
 formattersManager = RecordFormatter()
 
 
 #example formatter function
 def dummyFormatter(record):
+    """
+    This function does nothing, it just returns the record.
+    """
+    
     return record
-                    
 
 
 def foursquare_geojson(object):
+    """
+    Returns geojson object from a Foursquare data record.
+    Only the id attribute is passed in geometric feature property.
+    """
+    
     loc = object['location']
 
     properties = dict()
@@ -27,6 +41,8 @@ def foursquare_geojson(object):
     
     return out
 
+
+#registering the formatting functions
 formattersManager.registerFormattingFunction(dummyFormatter)
 formattersManager.registerFormattingFunction(foursquare_geojson)
 
--- sketchproject/sketch/recordformatter.py
+++ sketchproject/sketch/recordformatter.py
@@ -1,7 +1,15 @@
+"""
+recordformatter module
+
+"""
+
 from copy import deepcopy
 
 
 class RecordFormatter(object):
+    """
+    This class is a container for formatter functions.
+    """
 
     def __init__(self):
         self.formattingFunctions = {}
@@ -9,18 +17,24 @@ class RecordFormatter(object):
         
     def registerFormattingFunction(self, function):
         """
-        register a formatting function. 
+        registers a formatting function. 
+        It will be referenced by its name.
         """
         self.formattingFunctions[function.__name__] = function
+        
     
     def getFormatters(self):
         return self.formattingFunctions.keys()
+        
         
     def getFormatter(self, name):
         return self.formattingFunctions[name]
         
         
     def formatRecord(self, record, functionName, *args, **kwargs):
+        """
+        formats a record using the given function name
+        """
         
         pf = self.formattingFunctions[functionName]
         return pf(record, *args, **kwargs)
